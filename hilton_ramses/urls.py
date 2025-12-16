13150e38643af963af310e798427f237b4e47310
DesertCamel <35378010+MidoCobra@users.noreply.github.com>
2025-12-16 05:00:41 +0200

    generate QR codes scan templates for different papers sizes, and matching admin panel with hotel name

--- hilton_ramses/urls.py
+++ hilton_ramses/urls.py
@@ -19,6 +19,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# Customize Django admin site
+admin.site.site_header = "Ramses Hilton Admin"
+admin.site.site_title = "Ramses Hilton"
+admin.site.index_title = "Welcome to Ramses Hilton Management"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('welcomeletter.urls')),
